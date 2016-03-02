b94b8270978d3b2883d9160158485983549da984
ErikAbrahamson <e.a.abrahamson@gmail.com>
2016-03-02 18:13:15 +0200

    incremental learning

--- labelTooltip.js
+++ labelTooltip.js
@@ -1,41 +1,14 @@
-prism.labelTooltip = function(widget, widgetArgs, labelSize) {
+prism.labelTooltip = function(widget, widgetArgs, size) {
 
-    // 1. Truncate Labels
-    widget.on("beforeviewloaded", function(e, args) {
-        var labelSize = labelSize || 10;
-
-        _.each(widgetArgs.seriesIndexes, function(i) {
-            var props = args.options.series;
-            if (props.length > i) {
-
-                // Truncate labels
-                console.log(props[i]);
-                var elipsys = props[i].label.length < 6 ? '' : '...';
-                props[i].label = props[i].label.substring(0, labelSize) + ellipsys;
-            }
-        });
+    widget.on('beforeviewloaded', function(w, args) {
+    	args.options.xAxis.labels.formatter = function() {
+    		var ellipsys = this.value.length > size ? '' : '...';
+    		return this.value + ellipsys;
+    	};
     });
 
-    //2. Build Tooltip data
     widget.on("beforedatapointtooltip", function(se, args) {
-        var category = args.context.pointScope.selectionData[0], points = [];
-        if (widgetArgs.displayCurrentSelection) points = args.context.points;
-
-        _.each(widgetArgs.seriesIndexes, function(i) {
-            var e = args.widget.queryResult.series[i];
-
-            for (var j = 0; j < e.data.length; ++j) {
-                if (e.data[j].selectionData[0] == category) {
-                    points.push({
-                        seriesName: e.name,
-                        showPercentage: false,
-                        value: e.data[j].y.toLocaleString(),
-                        valueColor: e.data[j].color
-                    });
-                }
-            }
-        });
-
-        args.context.points = points;
+        var dom = prism.$injector.get('ux-controls.services.$dom');
+        console.log(args.context.points);
     });
 };
