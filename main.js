43ea5d4fff7e94618c140a879067cf5866ad0622
ErikAbrahamson <e.a.abrahamson@gmail.com>
2016-03-09 22:58:06 -0700

    fix syntax

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 (function(ns) {
     'use strict';
-    
+
     ns.labelTruncate = function(w, size) {
+
+        var supportedChartTypes = ['chart/column'];
+        function widgetIsSupported(type) {
+            if (supportedChartTypes.indexOf(type) >= 0) return true;
+            else return false;
+        }
+
     	w.on('beforeviewloaded', function(w, args) {
             size = size || 10;
     		args.options.xAxis.labels.formatter = function() {
